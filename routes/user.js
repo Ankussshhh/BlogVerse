405693595cc15eb288a228a2c05c77101847b057
Ankush Patil <ankushpatil2002@gmail.com>
2025-01-18 18:40:43 +0530

    handeled userAuth for Signin

--- routes/user.js
+++ routes/user.js
@@ -3,30 +3,34 @@ const User = require("../models/user");
 
 const router = Router();
 
-router.get("/signin", (req,res) => {
-    return res.render("signin")
-})
+router.get("/signin", (req, res) => {
+  return res.render("signin");
+});
 
-router.get("/signup", (req,res) => {
-    return res.render("signup")
-})
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
 
-router.post("/signup", async(req,res) => {
-    const {fullName , email , password} = req.body;
-    await User.create({
-        fullName,
-        email,
-        password
-    });
-    return res.redirect("/");
-})
+router.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  await User.create({
+    fullName,
+    email,
+    password,
+  });
+  return res.redirect("/");
+});
 
-router.post("/signin", async(req,res) => {
-    const {email , password} = req.body;
-    const user = await User.matchPassword(email,password);
-    console.log("User ", user);
-    
-    return res.redirect("/");
-})
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+        error : "Incorrect Email or Password"
+    })
+  }
+});
 
 module.exports = router;
